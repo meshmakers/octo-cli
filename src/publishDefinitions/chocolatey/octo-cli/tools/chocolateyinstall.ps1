ebe9dd9251de1e319b6bb891ceb60d5033a81e23
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-09-09 15:52:45 +0200

    Added sha512 checksum for zip file.

--- src/publishDefinitions/chocolatey/octo-cli/tools/chocolateyinstall.ps1
+++ src/publishDefinitions/chocolatey/octo-cli/tools/chocolateyinstall.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64      = 'https://meshdownload.blob.core.windows.net/staging/octo-cli/octo-cli-__VERSION__-win-x64.zip'
+$checksum64 = "__CHECKSUM__"
+$checksumType64 = "sha512"
 
 Install-ChocolateyZipPackage -PackageName $packageName -UnzipLocation $toolsDir -Url64 $url64 -checksum64 $checksum64 -checksumType64 $checksumType64
 
