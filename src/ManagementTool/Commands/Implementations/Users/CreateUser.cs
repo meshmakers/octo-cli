d938d8e7b13b7fe363382341513e9d678f52fd13
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-12 14:32:30 +0100

    AB#1038: Create user does not allow to create roles any more.

--- src/ManagementTool/Commands/Implementations/Users/CreateUser.cs
+++ src/ManagementTool/Commands/Implementations/Users/CreateUser.cs
@@ -1,7 +1,6 @@
 using Meshmakers.Common.CommandLineParser;
 using Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
 using Meshmakers.Octo.Frontend.ManagementTool.Services;
-using Meshmakers.Octo.Sdk.ServiceClient;
 using Meshmakers.Octo.Sdk.ServiceClient.IdentityServices;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -13,7 +12,6 @@ internal class CreateUser : ServiceClientOctoCommand<IIdentityServicesClient>
     private readonly IArgument _eMailArg;
     private readonly IArgument _nameArg;
     private readonly IArgument _passwordArg;
-    private readonly IArgument _roleArg;
 
     public CreateUser(ILogger<CreateUser> logger, IOptions<OctoToolOptions> options,
         IIdentityServicesClient identityServicesClient, IAuthenticationService authenticationService)
@@ -24,8 +22,6 @@ internal class CreateUser : ServiceClientOctoCommand<IIdentityServicesClient>
             true, 1);
         _nameArg = CommandArgumentValue.AddArgument("un", "userName", ["User name"], true,
             1);
-        _roleArg = CommandArgumentValue.AddArgument("r", "role", ["Role of user"], true,
-            1);
         _passwordArg = CommandArgumentValue.AddArgument("p", "password", ["Password"], false,
             0);
     }
@@ -35,30 +31,15 @@ internal class CreateUser : ServiceClientOctoCommand<IIdentityServicesClient>
     {
         var eMail = CommandArgumentValue.GetArgumentScalarValue<string>(_eMailArg).ToLower();
         var name = CommandArgumentValue.GetArgumentScalarValue<string>(_nameArg).ToLower();
-        var roleName = CommandArgumentValue.GetArgumentScalarValue<string>(_roleArg).ToLower();
         var password = CommandArgumentValue.GetArgumentScalarValueOrDefault<string>(_passwordArg);
 
         Logger.LogInformation("Creating user \'{Name}\' at \'{ServiceClientServiceUri}\'", name,
             ServiceClient.ServiceUri);
 
-        RoleDto roleDto;
-        try
-        {
-            roleDto = await ServiceClient.GetRoleByName(roleName);
-        }
-        catch (ServiceClientResultException)
-        {
-            Logger.LogError("Role \'{RoleName}\' does not exist at \'{ServiceClientServiceUri}\'", roleName,
-                ServiceClient.ServiceUri);
-            return;
-        }
-
-
         var userDto = new RegisterUserDto
         {
             Email = eMail,
             Name = name,
-            Roles = new[] { roleDto },
             Password = password
         };
 
