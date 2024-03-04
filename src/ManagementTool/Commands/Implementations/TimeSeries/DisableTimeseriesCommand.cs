09de9a48b1983bd970fc53260aa7a650428ac05c
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-03-04 18:27:35 +0100

    Include Commands for TS management

---
+++ src/ManagementTool/Commands/Implementations/TimeSeries/DisableTimeseriesCommand.cs
@@ -0,0 +1,37 @@
+﻿using Meshmakers.Octo.Frontend.ManagementTool.Services;
+using Meshmakers.Octo.Sdk.ServiceClient.TimeSeries;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Meshmakers.Octo.Frontend.ManagementTool.Commands.Implementations.TimeSeries;
+
+public class DisableTimeseriesCommand : ServiceClientOctoCommand<ITimeSeriesServicesClient>
+{
+    public DisableTimeseriesCommand(ILogger<ServiceClientOctoCommand<ITimeSeriesServicesClient>> logger,
+        IOptions<OctoToolOptions> options, ITimeSeriesServicesClient serviceClient,
+        IAuthenticationService authenticationService) : base(logger, "DisableTimeseries",
+        "Disable timeseries services for the current tenant.", options,
+        serviceClient, authenticationService)
+    {
+    }
+
+    public override async Task Execute()
+    {
+        if (string.IsNullOrWhiteSpace(Options.Value.TenantId))
+        {
+            Logger.LogError("TenantId is missing");
+            return;
+        }
+
+        
+        Logger.LogInformation("Disable timeseries for tenant \'{TenantId}\' at \'{ServiceClientServiceUri}\'",
+            Options.Value.TenantId,
+            ServiceClient.ServiceUri);
+
+        await ServiceClient.DisableAsync(Options.Value.TenantId);
+
+        Logger.LogInformation("Timeseries for tenant \'{ClientId}\' at \'{ServiceClientServiceUri}\' disabled",
+            Options.Value.TenantId,
+            ServiceClient.ServiceUri);
+    }
+}
---
+++ src/ManagementTool/Commands/Implementations/TimeSeries/EnableTimeseriesCommand.cs
@@ -0,0 +1,37 @@
+﻿using Meshmakers.Octo.Frontend.ManagementTool.Services;
+using Meshmakers.Octo.Sdk.ServiceClient.TimeSeries;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Meshmakers.Octo.Frontend.ManagementTool.Commands.Implementations.TimeSeries;
+
+public class EnableTimeseriesCommand : ServiceClientOctoCommand<ITimeSeriesServicesClient>
+{
+    public EnableTimeseriesCommand(ILogger<ServiceClientOctoCommand<ITimeSeriesServicesClient>> logger,
+        IOptions<OctoToolOptions> options, ITimeSeriesServicesClient serviceClient,
+        IAuthenticationService authenticationService) : base(logger, "EnableTimeseries",
+        "Enable timeseries services for the current tenant.", options,
+        serviceClient, authenticationService)
+    {
+    }
+
+    public override async Task Execute()
+    {
+        if (string.IsNullOrWhiteSpace(Options.Value.TenantId))
+        {
+            Logger.LogError("TenantId is missing");
+            return;
+        }
+
+        
+        Logger.LogInformation("Enable timeseries for tenant \'{TenantId}\' at \'{ServiceClientServiceUri}\'",
+            Options.Value.TenantId,
+            ServiceClient.ServiceUri);
+
+        await ServiceClient.EnableAsync(Options.Value.TenantId);
+
+        Logger.LogInformation("Timeseries for tenant \'{ClientId}\' at \'{ServiceClientServiceUri}\' enabled",
+            Options.Value.TenantId,
+            ServiceClient.ServiceUri);
+    }
+}
