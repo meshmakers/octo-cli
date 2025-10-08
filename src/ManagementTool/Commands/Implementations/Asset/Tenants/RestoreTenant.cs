911a8f8b425c94b6570a32639fb63afa01e987ed
Reimar Klammer <reimar.klammer@meshmakers.io>
2025-10-08 16:36:20 +0200

    AB#2703 New: Tenants can be restored with other than original database name

--- src/ManagementTool/Commands/Implementations/Asset/Tenants/RestoreTenant.cs
+++ src/ManagementTool/Commands/Implementations/Asset/Tenants/RestoreTenant.cs
@@ -12,6 +12,7 @@ internal class RestoreTenant : JobWithWaitOctoCommand
     private readonly IArgument _databaseArg;
     private readonly IArgument _tenantIdArg;
     private readonly IArgument _fileArg;
+    private readonly IArgument _oldDatabaseNameArg;
 
     public RestoreTenant(ILogger<RestoreTenant> logger, IOptions<OctoToolOptions> options,
         IBotServicesClient botServicesClient, IAuthenticationService authenticationService)
@@ -24,6 +25,7 @@ internal class RestoreTenant : JobWithWaitOctoCommand
         _databaseArg = CommandArgumentValue.AddArgument("db", "database", ["Name of database"], true,
             1);
         _fileArg = CommandArgumentValue.AddArgument("f", "file", ["File of backup (*.tar.gz)"], true, 1);
+        _oldDatabaseNameArg = CommandArgumentValue.AddArgument("oldDb", "oldDatabaseName", ["Name of the old database (if different to new database name)"], false, 1);
     }
 
     public override async Task Execute()
@@ -31,10 +33,11 @@ internal class RestoreTenant : JobWithWaitOctoCommand
         var tenantId = CommandArgumentValue.GetArgumentScalarValue<string>(_tenantIdArg).ToLower();
         var databaseName = CommandArgumentValue.GetArgumentScalarValue<string>(_databaseArg).ToLower();
         var filePath = CommandArgumentValue.GetArgumentScalarValue<string>(_fileArg);
+        var oldDatabaseName = CommandArgumentValue.GetArgumentScalarValue<string?>(_oldDatabaseNameArg)?.ToLower();
 
         Logger.LogInformation(
-            "Restoring tenant \'{TenantId}\' (database \'{DatabaseName}\') at \'{ServiceClientServiceUri}\'", tenantId,
-            databaseName, ServiceClient.ServiceUri);
+            "Restoring tenant \'{TenantId}\' (database \'{DatabaseName}\') at \'{ServiceClientServiceUri}\'. Old database name: \'{oldDatabaseName}\'", tenantId,
+            databaseName, ServiceClient.ServiceUri, oldDatabaseName ?? databaseName);
 
         if (string.IsNullOrEmpty(tenantId))
         {
@@ -45,8 +48,8 @@ internal class RestoreTenant : JobWithWaitOctoCommand
         {
             throw ToolException.FilePathDoesNotExist(filePath);
         }
-
-        var response = await ServiceClient.RestoreRepositoryAsync(tenantId, databaseName, filePath);
+        
+        var response = await ServiceClient.RestoreRepositoryAsync(tenantId, databaseName, filePath, oldDatabaseName);
         Logger.LogInformation("Run restore with job id \'{Id}\' has been started", response.JobId);
         await WaitForJob(response.JobId);
         Logger.LogInformation(
